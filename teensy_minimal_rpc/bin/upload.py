85489781315be6d232ee554b0e1dc1b57f383f19
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2025-06-05 16:43:33 -0400

    update upload.py

--- teensy_minimal_rpc/bin/upload.py
+++ teensy_minimal_rpc/bin/upload.py
@@ -1,10 +1,20 @@
 # coding: utf-8
-from arduino_helpers.upload import upload, parse_args
-from .. import get_firmwares
+from platformio_helpers.upload import upload_conda
+import platformio_helpers as pioh
 
 
 if __name__ == '__main__':
-    args = parse_args()
+    from argparse import ArgumentParser
 
-    print(upload(args.board_name, lambda b: get_firmwares()[b][0], args.port,
-                 args.arduino_install_home, verify=not args.skip_verify))
+    environments = sorted([dir_i.name for dir_i in pioh.conda_bin_path()
+                          .joinpath('teensy-minimal-rpc').dirs()])
+
+    parser = ArgumentParser(description='Upload firmware to board.')
+    parser.add_argument('-p', '--port', default=None)
+    parser.add_argument('-b', '--hardware-version', default=environments[-1],
+                        choices=environments)
+    args = parser.parse_args()
+    extra_args = [] if args.port is None else ['--port', args.port]
+
+    upload_conda('teensy-minimal-rpc',
+                 env_name=args.hardware_version, extra_args=extra_args)
